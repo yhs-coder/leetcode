2734ed95d611822c6a00b95d683d24153260661a
yhs-coder <yhs6666@outlook.com>
2024-01-02 23:29:30 +0800

    leetcode383. 赎金信

---
+++ Hashtable/RansomNote.cpp
@@ -0,0 +1,28 @@
+/*
+source: https://leetcode.cn/problems/ransom-note/description/
+leetcode383. 赎金信
+*/
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int record[26] = {0};
+        //add
+        if (ransomNote.size() > magazine.size()) {
+            return false;
+        }
+        for (int i = 0; i < magazine.length(); i++) {
+            // 通过record数据记录 magazine里各个字符出现次数
+            record[magazine[i]-'a'] ++;
+        }
+        for (int j = 0; j < ransomNote.length(); j++) {
+            // 遍历ransomNote，在record里对应的字符个数做--操作
+            record[ransomNote[j]-'a']--;
+            // 如果小于零说明ransomNote里出现的字符，magazine没有
+            if(record[ransomNote[j]-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
